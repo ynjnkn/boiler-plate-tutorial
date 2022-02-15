6e5ba083fe7cc5f60035b323f6f0f2a784088e2b
ynjnkn <yjun_k@hotmail.com>
2022-02-15 14:01:17 +0900

    노드 리액트 기초 강의 #13 Auth 기능 만들기 - 완료

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 
 const { User } = require('./models/User');
 
@@ -23,6 +24,30 @@ mongoose
 
 app.get("/", (req, res) => {
     res.send("보일러 플레이트 튜토리얼");
+});
+
+// auth 라우터의 기능
+// 1. 페이지 이동 때마다 로그인 여부, 관리자 유저 여부 체크
+// 2. 특정 기능들에 대한 권한이 있는지 여부 체크 (글 생성, 글 삭제 ...)
+
+// auth 구현 순서
+// 1. 쿠키에 저장된 token을 서버에서 가져와서 decode
+// 2. decode된 user id로 유저DB에 저장된 유저를 찾은 후, 쿠키의 토큰과 유저DB의 토큰이 동일한지 확인
+// 3. 
+
+// auth 라우터
+app.get('/app/users/auth', auth, (req, res) => {
+    // auth 미들웨어에서 auth 검증 완료된 상태
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
+    });
 });
 
 // 회원가입 라우터
@@ -73,6 +98,7 @@ app.post('/api/users/login', (req, res) => {
                 };
                 // 토큰을 쿠키에 저장
                 console.log("쿠키에 토큰 저장");
+                console.log("user.token", user.token);
                 res.cookie("x_auth", user.token)
                     .status(200)
                     .json({ loginSuccess: true, userId: user._id });
--- models/User.js
+++ models/User.js
@@ -88,6 +88,16 @@ userSchema.methods.generateToken = function (callback) {
     });
 }
 
+userSchema.statics.findByToken = function (token, callback) {
+    var user = this;
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return callback(error);
+            callback(null, user);
+        })
+    });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
