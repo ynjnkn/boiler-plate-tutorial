04a7636b835d303ff416b7e90fdfff23c4844ff8
ynjnkn <yjun_k@hotmail.com>
2022-02-15 13:16:44 +0900

    노드 리액트 기초 강의 #12 토큰 생성 with jsonwebtoken - 완료

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose
@@ -24,17 +26,59 @@ app.get("/", (req, res) => {
 });
 
 // 회원가입 라우터
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
+    console.log(`${req.body.name} 회원가입 시작`);
     const user = new User(req.body);
-
     user.save((err, doc) => {
+        console.log("save() 실행");
         if (err) {
             return res.json({ success: false, err });
         }
-        return res.status(200).json({
-            success: true
-        });
+        console.log(`${req.body.name} 회원가입 완료`);
+        return res
+            .status(200)
+            .json({
+                success: true
+            });
     })
+});
+
+// 로그인 라우터
+app.post('/api/users/login', (req, res) => {
+    // DB에서 요청한 이메일 찾기
+    User.findOne({ email: req.body.email }, (err, user) => {
+        console.log(`${req.body.email}에 대한 findOne() 실행`);
+        if (!user) {
+            console.log(`${req.body.email}에 해당하는 유저 없음`);
+            return res.json({
+                loginSucess: false,
+                message: "입력된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        console.log(`${req.body.email}에 해당하는 유저 있음`);
+        // 요청한 이메일이 DB에 있다면? 비밀번호가 같은지 확인
+        user.comparePassword(req.body.password, (err, isMatched) => {
+            console.log("콜백 comparePassword() 실행 완료");
+            if (!isMatched) {
+                console.log("비밀번호 일치하지 않음");
+                return res.json({ loginSuccess: false, message: "잘못된 비밀번호 입니다." });
+            };
+            // 비밀번호가 같다면 토큰 생성
+            console.log("비밀번호 일치함. 토큰 생성 시작");
+            user.generateToken((err, user) => {
+                console.log("콜백 generateToken() 실행 완료");
+                if (err) {
+                    console.log("토큰 생성 실패");
+                    return res.status(400).send(err);
+                };
+                // 토큰을 쿠키에 저장
+                console.log("쿠키에 토큰 저장");
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            });
+        });
+    });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,6 +36,7 @@ const userSchema = mongoose.Schema({
     }
 })
 
+// 회원가입 라우터에서 save() 실행 전의 동작들
 userSchema.pre('save', function (next) {
     // 위의 userSchema에서 client로부터 입력받은 사용자 정보 불러오기
     var user = this;
@@ -44,13 +46,47 @@ userSchema.pre('save', function (next) {
         bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err);
             bcrypt.hash(user.password, salt, function (err, hash) {
+                console.log(`${user.name}의 비밀번호 hashing 시작`);
                 if (err) return next(err);
                 user.password = hash;
+                console.log(`${user.name}의 비밀번호 hashing 완료`);
                 next();
+                console.log(`next() 실행`);
             })
         });
+    }
+    // password에 변동사항 없으므로 다음 미들웨어 호출
+    else {
+        next();
     };
 })
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+    console.log("콜백 comparePassword() 실행");
+    // 입력받은 plainPasswor와 DB의 hashing된 비밀번호가 같은지 비교
+    bcrypt.compare(plainPassword, this.password, function (err, isMatched) {
+        console.log("isMatched", isMatched);
+        if (err) return callback(err);
+        callback(null, isMatched);
+    })
+}
+
+userSchema.methods.generateToken = function (callback) {
+    console.log("콜백 generateToken() 실행");
+    var user = this;
+    // jsonwebtoken을 이용해서 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    console.log("user", user);
+    // 해당 유저의 토큰 저장 1
+    user.token = token;
+    console.log("user", user);
+    // 해당 유저의 토큰 저장 2 (왜 한 번 더 저장?)
+    user.save(function (err, user) {
+        console.log("user", user);
+        if (err) return callback(err);
+        callback(null, user);
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
