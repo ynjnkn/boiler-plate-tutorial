1af3bc81ac3716299ffda5aacaefae125de432a8
ynjnkn <yjun_k@hotmail.com>
2022-02-15 19:44:28 +0900

    노드 리액트 기초 강의 #20 React Router Dom - 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage.js";
+import LoginPage from "./components/views/LoginPage/LoginPage.js";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter >
   );
 }
 
 export default App;
+
+
